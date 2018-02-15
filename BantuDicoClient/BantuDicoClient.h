25e0192d7b248cda03f4fd738de4c403d5e34687
Mohamed Aymen Landolsi <mohamed.aymen.landolsi@gmail.com>
2018-02-15 17:21:49 +0100

    Created xcodeproj

---
+++ BantuDicoClient/BantuDicoClient.h
@@ -0,0 +1,19 @@
+//
+//  BantuDicoClient.h
+//  BantuDicoClient
+//
+//  Created by Mohamed Aymen Landolsi on 15/02/2018.
+//  Copyright © 2018 Rokridi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BantuDicoClient.
+FOUNDATION_EXPORT double BantuDicoClientVersionNumber;
+
+//! Project version string for BantuDicoClient.
+FOUNDATION_EXPORT const unsigned char BantuDicoClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BantuDicoClient/PublicHeader.h>
+
+
